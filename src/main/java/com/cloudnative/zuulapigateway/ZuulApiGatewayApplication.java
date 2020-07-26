6f47a519d0f3cfb3b92edebc1754e30b8612d2d6
Muralidharan Balanandan <muralidharan.rade@gmail.com>
2020-07-26 19:54:30 +0530

    zuul filters

--- src/main/java/com/cloudnative/zuulapigateway/ZuulApiGatewayApplication.java
+++ src/main/java/com/cloudnative/zuulapigateway/ZuulApiGatewayApplication.java
@@ -2,10 +2,12 @@ package com.cloudnative.zuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulServer
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
