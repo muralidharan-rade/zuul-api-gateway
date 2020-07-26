6eb1920e9a1b64f744c1dd7ae643acca5a0546bc
Muralidharan Balanandan <muralidharan.rade@gmail.com>
2020-07-26 17:47:27 +0530

    basic config

--- src/main/java/com/cloudnative/zuulapigateway/ZuulApiGatewayApplication.java
+++ src/main/java/com/cloudnative/zuulapigateway/ZuulApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.cloudnative.zuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableZuulServer
 public class ZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
